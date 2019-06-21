56a9b08076fd5fcd23a9ca2fef4e6f7c6cdd7a3e
Kirsten Floyd <45470377+floydkirsten@users.noreply.github.com>
2019-06-21 14:30:40 -0400

    Add files via upload

---
+++ server.js
@@ -0,0 +1,13 @@
+var http = require('http');
+var fs = require('fs');
+
+http.createServer(function(req, res) {
+    function callback(err, data) {
+        if (err) throw err;
+        res.writeHead(200, { 'Content-Type': 'text/html' });
+        res.write(data);
+        res.end();
+    }
+
+    fs.readFile('./display.html', callback);
+}).listen(8080);
