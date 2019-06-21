8e82c0ba3aa388979de5b33da3a5638c6870b9ec
kirstenflo <floyd.kirsten@yahoo.com>
2019-06-21 15:11:28 -0400

    -

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ var fs = require('fs');
 http.createServer(function(req, res) {
 
     function callback(err, data) {
+        console.log(req);
         if (err) throw err;
         res.writeHead(200, { 'Content-Type': 'text/html' });
         res.write(data);
