2829a17e5db67aa29bd453629e78a514097746dc
JamesWensel <jdwensel@ksu.edu>
2019-06-25 08:59:37 -0400

    Fix CSS/JS issue

--- server.js
+++ server.js
@@ -4,10 +4,23 @@ var fs = require('fs');
 http.createServer(function(req, res) {
     function callback(err, data) {
         if (err) throw err;
-        res.writeHead(200, { 'Content-Type': 'text/html' });
+        let contentType = "text/html"; 
+        if (req.url == '/display.css') contentType = "text/css";
+        if (req.url == '/server.js') contentType = "js";
+        console.log(contentType);
+        res.writeHead(200, { 'Content-Type': contentType });
         res.write(data);
         res.end();
     }
+    if (req.url == '/') fs.readFile('./display.html', callback)
+    else if (req.url != '/favicon.ico'){
+        console.log(req.url); 
+        fs.readFile("." + req.url, callback);
+    }
+        
+    
+}).listen(8000);
 
-    fs.readFile('./display.html', callback);
-}).listen(8080);
+function hello() {
+    console.log("Hello World");
+}
